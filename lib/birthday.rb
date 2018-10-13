664238c9b4c2fb1982ea51a18432e2238264b026
SANJAY K SAI <shachsan@hotmail.com>
2018-10-13 02:31:25 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,12 +7,3 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each {|kid, age|puts "Happy Birthday #{kid}! You are now #{age} years old!"}
 end
-
-# def select_winner(passengers)
-#   passengers.each do |suite, name|
-#     if suite == :suite_a && name.start_with?("A")
-#       return name
-#     end
-#   end
-#
-# end
