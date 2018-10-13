3e48438e1f5a76d2ec5daced93753578b8dea1a0
SANJAY K SAI <shachsan@hotmail.com>
2018-10-13 02:31:20 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  def select_winner(passengers)
+    passengers.each do |suite, name|
+      if suite == :suite_a && name.start_with?("A")
+        return name
+      end
+    end
+  
+  end
 end
